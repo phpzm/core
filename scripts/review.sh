4bfa1752ca298a88e758ffde2d50d55e592ecdf2
William Correa <wilcorrea@gmail.com>
2017-04-06 19:17:13 -0300

    Update version [Qui Abr  6 19:17:13 BRT 2017]

--- scripts/review.sh
+++ scripts/review.sh
@@ -7,9 +7,6 @@ shopt -s dotglob
 find * -prune -type d | while read d; do
     cd "$d"
     echo -n "$d"
-    if [ -f ".dirty" ]; then
-        rm ".dirty"
-    fi
     output=$(git status)
     if [[ ${output} == *"Changes not staged for commit"* ]]; then
         echo -n "*"
