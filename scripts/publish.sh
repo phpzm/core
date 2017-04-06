34f868a439f4c40ddc1a88547d5207c0b8505e47
William Correa <wilcorrea@gmail.com>
2017-04-06 19:08:18 -0300

    Update version [Qui Abr  6 19:08:18 BRT 2017]

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,13 +6,13 @@ version=$1
 
 shopt -s dotglob
 find * -prune -type d | while read d; do
+    cd "$d"
     if [ -f ".dirty" ]; then
-        cd "$d"
         tag=$(grep -Po '(?<="version": ")[^"]*' composer.json)
         echo "\"phpzm/$d\": \"$tag\""
         git tag "${tag}"
         git push origin "tags/${tag}" --quiet
         rm ".dirty"
-        cd ../
     fi
+    cd ../
 done
--- scripts/update.sh
+++ scripts/update.sh
@@ -12,11 +12,14 @@ shopt -s dotglob
 find * -prune -type d | while read d; do
     cd "$d"
     if [[ -f ".dirty" ]]; then
-        version=$(grep -Po '(?<="version": ")[^"]*' composer.json)
-        echo "phpzm/$d ~> $version"
-        git add --all
-        git commit -m "Update version [$(date)]" --quiet
-        git push origin master --quiet
+        output=$(git status)
+        if [[ ${output} == *"Changes not staged for commit"* ]]; then
+            version=$(grep -Po '(?<="version": ")[^"]*' composer.json)
+            echo "phpzm/$d ~> $version"
+            git add --all
+            git commit -m "Update version [$(date)]" --quiet
+            git push origin master --quiet
+        fi
     fi
     cd ../
 done
